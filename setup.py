72809db9ed91c11f810463c54ccc00abd5c0c691
Makram Kamaleddine <makram.kd@gmail.com>
2020-04-18 11:41:52 -0400

    Update setup.py with coverage requirement

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 from setuptools import setup, find_packages
 import unittest
 
@@ -16,6 +17,15 @@ setup(
     description='Simple environment variable configuration for Python',
     author='Makram Kamaleddine',
     url='github.com/makramkd/envconfig',
+    keywords='environment-variables env-var configuration',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ],
     packages=find_packages(),
     test_suite='setup.test_suite',
+    tests_require=[
+        'coverage',
+    ],
 )
